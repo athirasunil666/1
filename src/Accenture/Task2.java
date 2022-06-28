c775602afccc450659221576d815fa3a4f452c95
Athira Sunil <athirasunil457@gmail.com>
2022-06-28 15:52:35 +0300

    Initial commit

---
+++ src/Accenture/Task2.java
@@ -0,0 +1,24 @@
+package Accenture;
+
+public class Task2 {
+public static void main(String[] args) {
+    int a = 10;
+    int b = 10;
+
+    if (a == b) {
+        System.out.println( "is 10 equal to 10? - true" );
+    }
+    if (a >= b) {
+        System.out.println( "is 10 greater or equal to 10? - true" );}
+        else if (a > b){
+        System.out.println( "is 10 greater than 10? - false" );
+    }
+    if (a <= b) {
+    System.out.println( "is 10 less or equal to 10? - true" );}
+    else if (a < b) {
+        System.out.println( "is 10 less than 10? - false" );
+    }
+}
+    }
+
+
